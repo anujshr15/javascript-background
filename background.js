0ea44c195ae9de2a549d2148431eeea283704567
anujshr15 <anujshrivastav97@gmail.com>
2018-08-22 10:58:41 +0530

    changing html and js file

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+var _=require("lodash");
+
+var array=[1,2,3,4,5];
+console.log("answer= ",_.without(array,4));
+
+
 var h3=document.querySelector("h3");
 var color1=document.getElementById("color1");
 var color2=document.getElementById("color2");
